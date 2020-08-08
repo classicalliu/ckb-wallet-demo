4ae5a7af5c0b8c14fbfe97cbb583dddb9d92d917
classicalliu <classicalliu@gmail.com>
2020-08-08 20:33:38 +0800

    Watch tsc & node

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,7 +3,12 @@ let router = express.Router();
 
 /* GET users listing. */
 router.get("/", (_req, res, _next) => {
-  res.send("respond with a resource");
+  // res.send("respond with a resource");
+  res.send({
+    one: 1,
+    two: 2,
+    three: 3,
+  });
 });
 
 export default router;
