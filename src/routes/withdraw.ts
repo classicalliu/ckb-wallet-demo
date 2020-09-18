05ae5836d2c3718a54e14ba35908154bf152eb68
classicalliu <classicalliu@gmail.com>
2020-09-18 23:48:22 +0800

    feat: Add sudt router

--- src/routes/withdraw.ts
+++ src/routes/withdraw.ts
@@ -20,4 +20,25 @@ router.post("/", async (req, res, _next) => {
   });
 });
 
+router.post("/sudt", async (req, res, _next) => {
+  const address: string = req.body.address;
+  const sudtAmount: bigint = BigInt(req.body.sudt_amount);
+  const sudtToken: string = req.body.sudt_token;
+
+  const transaction = new Transaction();
+  const result: number = await transaction.withdraw(
+    getAccountId(req, res),
+    address,
+    undefined,
+    {
+      sudtAmount,
+      sudtToken,
+    }
+  );
+
+  res.json({
+    id: result,
+  });
+});
+
 export default router;
