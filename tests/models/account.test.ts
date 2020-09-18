0964efc1593994058b9eae46187a1fac8a9545d8
classicalliu <classicalliu@gmail.com>
2020-09-19 00:36:31 +0800

    Remove password tests

--- tests/models/account.test.ts
+++ tests/models/account.test.ts
@@ -3,24 +3,13 @@ import Account from "../../src/models/account";
 import { generateConnection } from "../../src/db/connection";
 
 const testUsername = "username";
-const testPassword: string = "password";
-
-test("encrypt & compare", async (t) => {
-  const account = new Account();
-  // @ts-ignore: Private method
-  const digest = await account.encryptPassword(testPassword);
-  // @ts-ignore: Private method
-  const result = await account.checkPassword(testPassword, digest);
-
-  t.is(result, true);
-});
 
 test("create", async (t) => {
   const knex = generateConnection();
   await knex.migrate.latest();
 
   const account = new Account(knex);
-  await account.create(testUsername, testPassword);
+  await account.create(testUsername);
   const result = await knex.select("*").from("accounts");
 
   t.is(result.length, 1);
@@ -35,7 +24,7 @@ test("get", async (t) => {
   const result = await account.get(testUsername);
   t.is(result, undefined);
 
-  await account.create(testUsername, testPassword);
+  await account.create(testUsername);
   const result2 = await account.get(testUsername);
 
   t.is(result2.username, testUsername);
