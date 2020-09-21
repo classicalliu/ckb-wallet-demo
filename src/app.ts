cd5f9d37e534a6f67827a76e087b9fb4bd4694c0
classicalliu <classicalliu@gmail.com>
2020-09-21 15:08:08 +0800

    Remove jade

--- src/app.ts
+++ src/app.ts
@@ -26,8 +26,8 @@ import { init } from "./models/init";
 let app = express();
 
 // view engine setup
-app.set("views", path.join(__dirname, "../views"));
-app.set("view engine", "jade");
+// app.set("views", path.join(__dirname, "../views"));
+// app.set("view engine", "jade");
 
 app.use(logger("dev"));
 app.use(express.json());
